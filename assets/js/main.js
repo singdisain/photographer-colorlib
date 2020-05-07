17fd78eaf68ec1b9c9ff7b84a3472739d89b53ec
Dodo Jaya Raditya <singdisain@gmail.com>
2020-05-08 01:48:15 +0700

    Update for hero slider & social menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,19 @@
  ---------------------------------------
  =======================================*/
 
-"use strict";
+'use strict';
 
-$(window).on("load", function () {
-  /*------------------
+$(window).on('load', function () {
+	/*------------------
 		Preloder
 	--------------------*/
   $(".loader").fadeOut();
   $("#preloder").delay(400).fadeOut("slow");
 
-  /*---------------------
+	/*---------------------
 		Instagram slider
 	----------------------*/
-  $(".instagram-slider").owlCarousel({
+  $('.instagram-slider').owlCarousel({
     nav: false,
     dots: false,
     loop: true,
@@ -32,6 +32,7 @@ $(window).on("load", function () {
       },
       480: {
         items: 4,
+
       },
       768: {
         items: 5,
@@ -41,86 +42,89 @@ $(window).on("load", function () {
       },
       1200: {
         items: 7,
-      },
-    },
+      }
+    }
   });
 
-  /*---------------
+	/*---------------
 		Masonry
 	----------------*/
   var masonryLayout = function () {
-    $(".portfolio-grid").masonry({
-      itemSelector: ".grid-item",
-      columnWidth: ".grid-sizer",
-      percentPosition: true,
+    $('.portfolio-grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true
     });
-  };
+  }
 
-  /*---------------
+	/*---------------
 		Mixitup
 	----------------*/
   masonryLayout();
-  if ($(".portfolio-gallery").length > 0) {
-    var containerEl = document.querySelector(".portfolio-gallery");
+  if ($('.portfolio-gallery').length > 0) {
+    var containerEl = document.querySelector('.portfolio-gallery');
     var mixer = mixitup(containerEl, {
       callbacks: {
         onMixEnd: function () {
           masonryLayout();
-        },
-      },
+        }
+      }
     });
   }
 });
 
 (function ($) {
-  /*------------------
+	/*------------------
 		Navigation
 	--------------------*/
-  $(".main-menu").slicknav({
-    appendTo: ".header-section",
+  $('.main-menu').slicknav({
+    appendTo: '.header-section',
     closedSymbol: '<i class="fa fa-angle-down"></i>',
-    openedSymbol: '<i class="fa fa-angle-up"></i>',
+    openedSymbol: '<i class="fa fa-angle-up"></i>'
   });
 
-  $(".nav-switch-btn").on("click", function () {
+  $('.nav-switch-btn').on('click', function () {
     if (localStorage.getItem("navMenu") == null) {
       localStorage.setItem("navMenu", "show");
-      $(".main-menu").slideDown(400);
+      $('.main-menu').slideDown(400);
     } else if (localStorage.getItem("navMenu") == "show") {
       localStorage.removeItem("navMenu");
-      $(".main-menu").slideUp(400);
+      $('.main-menu').slideUp(400);
     }
   });
 
   if (localStorage.getItem("navMenu") == "show") {
-    $(".main-menu").slideDown(400);
+    $('.main-menu').slideDown(400);
   }
 
-  /*------------------
+
+	/*------------------
 		Search model
 	--------------------*/
-  $(".search-btn").on("click", function () {
-    $(".search-model").fadeIn(400);
+  $('.search-btn').on('click', function () {
+    $('.search-model').fadeIn(400);
   });
 
-  $(".search-close-switch").on("click", function () {
-    $(".search-model").fadeOut(400, function () {
-      $("#search-input").val("");
+  $('.search-close-switch').on('click', function () {
+    $('.search-model').fadeOut(400, function () {
+      $('#search-input').val('');
     });
   });
 
-  /*------------------
+
+	/*------------------
 		Background Set
 	--------------------*/
-  $(".set-bg").each(function () {
-    var bg = $(this).data("setbg");
-    $(this).css("background-image", "url(" + bg + ")");
+  $('.set-bg').each(function () {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
   });
 
-  /*------------------
+
+	/*------------------
 		Hero Slider
 	--------------------*/
-  $(".hero-slider").owlCarousel({
+  $('.hero-slider').owlCarousel({
     nav: false,
     dots: false,
     loop: true,
@@ -132,6 +136,7 @@ $(window).on("load", function () {
       },
       480: {
         items: 2,
+
       },
       768: {
         items: 3,
@@ -144,108 +149,99 @@ $(window).on("load", function () {
       },
       1400: {
         items: 7,
-      },
-    },
+      }
+    }
   });
 
-  /*----------------------
+
+	/*----------------------
 		Portfolio item size
 	------------------------*/
   var PorfolioItemFix = function () {
     $(".portfolio-item").each(function (index) {
       var portfolioItem = $(this);
       var PIheight = portfolioItem.width();
-      portfolioItem.css("height", PIheight);
+      portfolioItem.css('height', PIheight);
     });
-  };
+  }
   PorfolioItemFix();
-  $(window).on("resize", function () {
+  $(window).on('resize', function () {
     PorfolioItemFix();
   });
 
-  /*------------------
+
+	/*------------------
 		Image Popup
 	--------------------*/
-  $(".img-popup").magnificPopup({
-    type: "image",
-    mainClass: "img-popup-warp",
+  $('.img-popup').magnificPopup({
+    type: 'image',
+    mainClass: 'img-popup-warp',
     removalDelay: 500,
   });
 
-  /*------------------
+
+	/*------------------
 		Progress Bar
 	--------------------*/
-  $(".progress-bar-style").each(function () {
+  $('.progress-bar-style').each(function () {
     var progress = $(this).data("progress");
-    var prog_width = progress + "%";
+    var prog_width = progress + '%';
     if (progress <= 100) {
-      $(this).append(
-        '<div class="bar-inner" style="width:' +
-          prog_width +
-          '"><span>' +
-          prog_width +
-          "</span></div>"
-      );
-    } else {
-      $(this).append(
-        '<div class="bar-inner" style="width:100%"><span>' +
-          prog_width +
-          "</span></div>"
-      );
+      $(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+    }
+    else {
+      $(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
     }
   });
 
-  /*------------------
+
+	/*------------------
 		Accordions
 	--------------------*/
-  $(".panel-link").on("click", function (e) {
-    $(".panel-link").parent(".panel-header").removeClass("active");
-    var $this = $(this).parent(".panel-header");
-    if (!$this.hasClass("active")) {
-      $this.addClass("active");
+  $('.panel-link').on('click', function (e) {
+    $('.panel-link').parent('.panel-header').removeClass('active');
+    var $this = $(this).parent('.panel-header');
+    if (!$this.hasClass('active')) {
+      $this.addClass('active');
     }
     e.preventDefault();
   });
 
-  /*------------------
+
+	/*------------------
 		Circle progress
 	--------------------*/
-  $(".circle-progress").each(function () {
+  $('.circle-progress').each(function () {
     var cpvalue = $(this).data("cpvalue");
     var cpcolor = $(this).data("cpcolor");
     var cptitle = $(this).data("cptitle");
     var cpid = $(this).data("cpid");
 
-    $(this).append(
-      '<div class="' +
-        cpid +
-        '"></div><div class="progress-info"><h2>' +
-        cpvalue +
-        "%</h2><p>" +
-        cptitle +
-        "</p></div>"
-    );
+    $(this).append('<div class="' + cpid + '"></div><div class="progress-info"><h2>' + cpvalue + '%</h2><p>' + cptitle + '</p></div>');
 
     if (cpvalue < 100) {
-      $("." + cpid).circleProgress({
-        value: "0." + cpvalue,
+
+      $('.' + cpid).circleProgress({
+        value: '0.' + cpvalue,
         size: 190,
         thickness: 3,
         fill: cpcolor,
-        emptyFill: "rgba(0, 0, 0, 0)",
+        emptyFill: "rgba(0, 0, 0, 0)"
       });
     } else {
-      $("." + cpid).circleProgress({
+      $('.' + cpid).circleProgress({
         value: 1,
         size: 190,
         thickness: 3,
         fill: cpcolor,
-        emptyFill: "rgba(0, 0, 0, 0)",
+        emptyFill: "rgba(0, 0, 0, 0)"
       });
     }
+
   });
 
-  /*------------------
+
+	/*------------------
 		Instafeed
 	--------------------*/
   var usrid = $(this).data("usrid");
@@ -262,7 +258,8 @@ $(window).on("load", function () {
       '<div class="instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
   });
 
-  if ($("#instafeed").length > 0) {
+  if ($('#instafeed').length > 0) {
     userFeed.run();
   }
+
 })(jQuery);
